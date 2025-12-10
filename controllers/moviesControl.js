7918a6cd3297abf6b592ee5e93493c0c63e3c0e3
calogerosorce <calogerosorce00@hotmail.com>
2025-12-10 12:33:26 +0100

    Implement index method to retrieve and return all movies from the database

--- controllers/moviesControl.js
+++ controllers/moviesControl.js
@@ -2,7 +2,12 @@ const connection = require('../database/connection')
 
 const index = (req, res) => {
 
-    res.send('Show all movies')
+    const sql = 'SELECT * FROM movies'
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: true, message: err.message })
+        res.json(results)
+    })
 }
 
 const show = (req, res) => {
